d47ae1397ecc7c769657832170e74d32fe7d0433
steel39 <quattro@ro.ru>
2024-01-25 16:56:10 +0200

    commit5.1

--- resources/js/views/LastTrades.vue
+++ resources/js/views/LastTrades.vue
@@ -1,19 +1,19 @@
 <template>
     <header class="flex flex-auto mt-2 columns-2">
-        <div class="w-full  content-center md:w-1/2">
-            <input class="appearance-none block w-full bg-pink-200 
-                text-gray-700 border border-gray-200 rounded py-3 px-4
+        <form class="columns-2 w-full content-center" @submit.prevent="">
+            <div class=" ">
+                <input class="appearance-none block w-full bg-pink-200 
+                text-gray-700 border border-gray-200 rounded py-3 
                  leading-tight focus:outline-none focus:bg-gray-300
-                 hover:shadow-emerald-300 focus:border-gray-500" id="grid-last-name"
-                type="text" placeholder="TICKER">
-        </div>
-        <div class="button">
-                <button  type="button" class="text-white bg-gradient-to-r
+                 hover:shadow-emerald-300 focus:border-gray-500 text-center" id="grid-last-name" type="text" placeholder="TICKER">
+            </div>
+            <button type="submit" class="text-white bg-gradient-to-r
              from-cyan-400 via-cyan-500 to-cyan-600
               hover:bg-gradient-to-l 
               focus:outline-none focus:ring-pink-300
                dark:focus:ring-cyan-800 font-medium rounded-lg
-                px-4 py-3 ml-3 text-center me-2 mb-2">Go LastTrades</button>
-        </div>
+                px-4 py-3  text-center me-2 ">Go LastTrades
+            </button>
+        </form>
     </header>
 </template>
