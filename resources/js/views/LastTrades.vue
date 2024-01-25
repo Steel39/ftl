7d8f344da6cee0b20d965f66170adba820caed12
steel39 <quattro@ro.ru>
2024-01-25 16:35:53 +0200

    commit5

--- resources/js/views/LastTrades.vue
+++ resources/js/views/LastTrades.vue
@@ -1,14 +1,19 @@
 <template>
-    <header class="flex flex-auto">
-    <div class="">
-        <div>
-            <div class="w-full content-center md:w-1/2 px-3">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-                    Input Ticker
-                </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="TICKER">
-            </div>
+    <header class="flex flex-auto mt-2 columns-2">
+        <div class="w-full  content-center md:w-1/2">
+            <input class="appearance-none block w-full bg-pink-200 
+                text-gray-700 border border-gray-200 rounded py-3 px-4
+                 leading-tight focus:outline-none focus:bg-gray-300
+                 hover:shadow-emerald-300 focus:border-gray-500" id="grid-last-name"
+                type="text" placeholder="TICKER">
         </div>
-    </div>
+        <div class="button">
+                <button  type="button" class="text-white bg-gradient-to-r
+             from-cyan-400 via-cyan-500 to-cyan-600
+              hover:bg-gradient-to-l 
+              focus:outline-none focus:ring-pink-300
+               dark:focus:ring-cyan-800 font-medium rounded-lg
+                px-4 py-3 ml-3 text-center me-2 mb-2">Go LastTrades</button>
+        </div>
     </header>
 </template>
