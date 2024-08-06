1a69b6a909d33d58efbd50fb0cd835b13f95a988
Steel39 <quattro@ro.ru>
2024-08-06 18:53:14 +0200

    add Inertia and auth

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,23 @@
+<script setup>
+import App from '@/App.vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import Stocks from '@/views/Stocks.vue';
+</script>
+
+<template>
+    <Head title="Панель управления" />
+    
+    <AuthenticatedLayout>
+        <template #header>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <Stocks>
+                
+            </Stocks>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,17 +1,24 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title class="text-blue">MOEXLab</title>
-    @vite('resources/css/tail.css')
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-</head>
-<body >
-<div id="app" class="conteiner absolute top-0 w-[100%] min-h-screen h-auto
-                     fixed bg-gradient-to-br  from-slate-700 to-zinc-800"
-                     ></div>
+        <title inertia>{{ config('app.name') }}</title>
 
-@vite('resources/js/app.js')
-</body>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @routes
+        @vite(['resources/js/app.js','resources/css/tail.css', "resources/js/Pages/{$page['component']}.vue"])
+        @inertiaHead
+    </head>
+    
+    <body class="conteiner absolute top-0 w-[100%] min-h-screen h-auto
+    fixed bg-gradient-to-br  from-slate-700 to-zinc-800"">
+        @inertia
+    </body>
 </html>
