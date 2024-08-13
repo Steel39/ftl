394feee65d94e88cd7cbafb65d7342344bb11aad
Steel39 <quattro@ro.ru>
2024-08-13 14:36:11 +0200

    edit conf18

--- docker/certbot/generate-certificate.sh
+++ docker/certbot/generate-certificate.sh
@@ -2,7 +2,7 @@
 # generate-certificate.sh
 
 # чистим папку, где могут находиться старые сертификаты
-rm -rf /etc/letsencrypt/live/certfolder*
+rm -rf /etc/letsencrypt/live/certfolder/
 
 # выдаем себе сертификат (обратите внимание на переменные среды)
 certbot certonly --standalone --email $DOMAIN_EMAIL -d $DOMAIN_URL --cert-name=certfolder --key-type rsa --agree-tos
@@ -13,5 +13,5 @@ rm -rf ./docker/nginx/cert.pem
 rm -rf ./docker/etc/nginx/key.pem
 
 # копируем сертификаты из образа certbot в папку Nginx
-cp /etc/letsencrypt/live/certfolder*/fullchain.pem ./docker/nginx/cert.pem
-cp /etc/letsencrypt/live/certfolder*/privkey.pem ./docker/etc/nginx/key.pem
+cp /etc/letsencrypt/live/certfolder/fullchain.pem ./docker/nginx/cert.pem
+cp /etc/letsencrypt/live/certfolder/privkey.pem ./docker/etc/nginx/key.pem
