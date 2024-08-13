0f085792cc1e5064ea655741ee826c7f3db83e4f
Steel39 <quattro@ro.ru>
2024-08-13 12:41:23 +0200

    edit conf11

--- docker/certbot/generate-certificate.sh
+++ docker/certbot/generate-certificate.sh
@@ -2,8 +2,8 @@
 # generate-certificate.sh
 
 # чистим папку, где могут находиться старые сертификаты
-rm -rf /etc/letsencrypt/live/moexlab.ru
-rm -rf /etc/letsencrypt/live/www.moexlab.ru
+rm -rf /etc/letsencrypt/live/moexlab.ru/
+rm -rf /etc/letsencrypt/live/www.moexlab.ru/
 
 # выдаем себе сертификат (обратите внимание на переменные среды)
 certbot certonly --standalone --email $DOMAIN_EMAIL -d $DOMAIN_URL --cert-name=certfolder --key-type rsa --agree-tos
